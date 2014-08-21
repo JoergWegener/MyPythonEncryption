febbd565af026573dac8b49ccaccfab6f6d23b22
Joerg Wegener <joerg.wegener@gmail.com>
2014-08-21 21:32:36 +0200

    Encryption works fine, decryption does not.
    Decryption returns "none".

--- EncryptionMatrix.py
+++ EncryptionMatrix.py
@@ -26,9 +26,12 @@ class EncryptionMatrix:
     
     # Main method of the class
     def convertText(self, inputText, direction):
+        
+        global MATRIXDIM
         outputText = ''
         
         for i in range(len(inputText) / 2):
+            
             c1 = inputText[i * 2]
             c2 = inputText[(i * 2) + 1]
             
@@ -40,17 +43,18 @@ class EncryptionMatrix:
             # If there is an uneven number of chars, the last one is moved
             # down / up one square
             if ((len(inputText) % 2) == 1):
-                c = inputText[inputText.length() - 1]
+                c = inputText[-1]
                 point = self.findPosition(c)
-                newPoint = MatrixPoint(point.x, ( point.y + direction.dirInd + EncryptionMatrix.MATRIXDIM ) % EncryptionMatrix.MATRIXDIM)
+                newPoint = MatrixPoint(point.x, ( point.y + direction + MATRIXDIM ) % MATRIXDIM)
                 outputText += self.getChar( newPoint)
         
-            return outputText
+        return outputText
             
         
     # Helper method that returns a string of 2 chars for the clear chars provided.
     # This result has to be added to the encrypted / decrypted string.
     def createTargetCharPair (self, point1, point2, direction):
+        global MATRIXDIM
         result = '' #This will contain the new string
         # Determine the relative position of the 2 characters
         if ((point1.x != point2.x) and (point1.y != point2.y)):
@@ -59,16 +63,16 @@ class EncryptionMatrix:
             result += self.getChar(MatrixPoint( point1.x, point2.y)) # move c2 horizontally
         elif (point1.x == point2.x) and (point1.y != point2.y): 
             # On the same X axis. Move both chars down / up one space; roll over if needed
-            result += self.getChar(MatrixPoint(point1.x, (point1.y + direction.dirInd + EncryptionMatrix.MATRIXDIM) % EncryptionMatrix.MATRIXDIM))
-            result += self.getChar(MatrixPoint(point2.x, (point2.y + direction.dirInd + EncryptionMatrix.MATRIXDIM) % EncryptionMatrix.MATRIXDIM))
+            result += self.getChar(MatrixPoint(point1.x, (point1.y + direction + MATRIXDIM) % MATRIXDIM))
+            result += self.getChar(MatrixPoint(point2.x, (point2.y + direction + MATRIXDIM) % MATRIXDIM))
         elif (point1.x != point2.x) and (point1.y == point2.y):
             # On the same Y axis. Move right / left one space; roll over if needed
-            result += self.getChar(MatrixPoint( point1.x + direction.dirInd + EncryptionMatrix.MATRIXDIM) % EncryptionMatrix.MATRIXDIM, point1.y)
-            result += self.getChar(MatrixPoint( point2.x + direction.dirInd + EncryptionMatrix.MATRIXDIM) % EncryptionMatrix.MATRIXDIM, point2.y)
+            result += self.getChar(MatrixPoint( ((point1.x + direction + MATRIXDIM) % MATRIXDIM), point1.y))
+            result += self.getChar(MatrixPoint( ((point2.x + direction + MATRIXDIM) % MATRIXDIM), point2.y))
         elif ((point1.x == point2.x) and (point1.y == point2.y)):
             # Identical characters: move both one space down / up, roll over if needed
-            result += self.getChar(MatrixPoint(point1.x, (point1.y + direction.dirInd + EncryptionMatrix.MATRIXDIM) % EncryptionMatrix.MATRIXDIM))
-            result += self.getChar(MatrixPoint(point2.x, (point2.y + direction.dirInd + EncryptionMatrix.MATRIXDIM) % EncryptionMatrix.MATRIXDIM))
+            result += self.getChar(MatrixPoint(point1.x, (point1.y + direction + MATRIXDIM) % MATRIXDIM))
+            result += self.getChar(MatrixPoint(point2.x, (point2.y + direction + MATRIXDIM) % MATRIXDIM))
         else:
             # SHIT this is not supposed to happen!
             pass
@@ -87,7 +91,7 @@ class EncryptionMatrix:
     # For testing purposes only!
     def printMatrix (self):
         for y in range(EncryptionMatrix.MATRIXDIM):
-            print(self.encryptionString.substring(y * EncryptionMatrix.MATRIXDIM, y * EncryptionMatrix.MATRIXDIM + EncryptionMatrix.MATRIXDIM - 1) + "\n")
+            print(self.encryptionString.substring(y * MATRIXDIM, y * MATRIXDIM + MATRIXDIM - 1) + "\n")
 
 
 
